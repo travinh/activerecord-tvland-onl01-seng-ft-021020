57c99dccc307095ebe703cb7d1285ba4fe28bf9f
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-04-21 06:29:40 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,18 @@
 class Actor < ActiveRecord::Base
+  has_many :characters
+  has_many :shows, through: :characters
+  
+  def full_name 
+    string = self.first_name
+    string = string +" " + self.last_name
+    string
+    # "#{self.first_name} #{self.last_name}"
+  end
+  
+  def list_roles
+    self.characters.map do |t|
+      "#{t.name} - #{t.show.name}"
+    end
+  end
   
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
+  belongs_to :network
+  has_many :characters
+  has_many :actors, through: :characters
+  
+  def actors_list
+    self.actors.collect do |t| 
+      "#{t.first_name} #{t.last_name}"
+    end
+  end
   
 end
