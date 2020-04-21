205e05dc6e906d39c7f492267cb69d52e0b733f8
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-04-21 06:07:24 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -2,4 +2,9 @@ class Actor < ActiveRecord::Base
   has_many :characters
   has_many :shows, through: :characters
   
+  def full_name 
+    string = self.first_name
+    string = string + self.last_name
+  end
+  
 end
